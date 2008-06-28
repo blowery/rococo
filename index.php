0310cc0b9521010c35d53f20fd7a069657f0fc6b
Ben Lowery <ben@blowery.org>
2008-06-27 21:59:40 -0400

    add in a sidebar with the meta hook

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
   <body>
     <h1>Awesome blog</h1>
+    <?php get_sidebar(); ?>
   </body>
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+<h2>This is my sidebar</h2>
+
+<h2>Meta</h2>
+<?php wp_meta(); ?>
+
+</div>
