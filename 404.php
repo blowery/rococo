97ecbb1bf23b6fc6f96d31dae560b9e74cef6f69
Ben Lowery <ben@blowery.org>
2008-06-28 21:08:09 -0400

    fix up calls in 404 handler

--- 404.php
+++ 404.php
@@ -1,4 +1,4 @@
-<?php wp_header(); ?>
+<?php get_header(); ?>
 <h1>Somebody needs a body massage.</h1>
-<?php wp_sidebar(); ?>
-<?php wp_footer(); ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
