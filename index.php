9a8b6d0d297bd7dd58304a57ac92d0271e18bc02
Ben Lowery <ben@blowery.org>
2008-07-03 13:15:10 -0400

    add an edit link to each post and reduce the font size of the meta line a touch.

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
           In <?php the_category(",") ?> on <abbr class="published" 
                               title="<?php the_time('c'); ?>"><?php the_time('M j Y'); ?></abbr>.
           <?php comments_popup_link("No comments.", "1 comment.", "% comments.", "comments", "Sorry, comments are closed."); ?>
+        <?php edit_post_link("Edit", '<span class="edit">', "</span>"); ?>
         </span></h2>
         <div class="entry-content">
           <?php the_content(); ?>
