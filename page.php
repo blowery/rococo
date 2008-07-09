e9b49c914a7277553a4052d5bc3ff776a027d6de
Ben Lowery <ben@blowery.org>
2008-07-09 15:06:00 -0400

    add in a page template

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<!-- html structure based on http://microformats.org/wiki/hatom -->
+<div id="content" class="hfeed page">
+  
+  <?php if(have_posts()) : ?>
+
+    <?php while (have_posts()) : the_post(); ?>
+      <div id="post-<?php the_ID(); ?>" class="post hentry">
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+        <span class="meta">
+          <abbr class="published" 
+                              title="<?php the_time('c'); ?>"><?php the_time('M j Y'); ?></abbr>.
+          <?php comments_popup_link("No comments.", "1 comment.", "% comments.", "comments", "Sorry, comments are closed."); ?>
+        <?php edit_post_link("Edit", '<span class="edit">', "</span>"); ?>
+        </span></h2>
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
+      </div>
+      
+      <?php comments_template(); ?>
+
+    <?php endwhile; else: ?>
+      <p class="no-posts">Hrm, nothing matched that.</p>
+  <?php endif; ?>
+
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
