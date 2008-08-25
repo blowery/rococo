34c41544d2c413aa03c671e733085e9bfa98bd2c
Ben Lowery <ben@blowery.org>
2008-08-25 19:42:13 +0000

    merge index and charting

--- index.php
+++ index.php
@@ -36,9 +36,6 @@
 
 <script type="text/javascript" src="/js/dojo/dojo.js"></script>
 <script type="text/javascript" src="/js/blowery/index.js"></script>
-
-<?php get_sidebar(); ?>
-<script type="text/javascript" src="/js/dojo/dojo.js"></script>
-<script type="text/javascript" src="/js/blowery/charting.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/roast-charts.js"></script>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
